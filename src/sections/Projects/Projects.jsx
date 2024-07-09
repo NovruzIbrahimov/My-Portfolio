5bb53c21e4f5d4318fc30cc0e2ff32331b795bc6
NovruzIbrahimov <novruz.ibrahimov56@gmail.com>
2024-07-09 15:08:39 +0400

    deleted some structure and added pictures or css

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,7 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
+import furniture from '../../assets/furniture2.png';
+import game from '../../assets/game4.png';
+import fashion from '../../assets/fashion4.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -11,28 +10,22 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Viberr"
-          p="Streaming App"
+          src={furniture}
+          link="https://github.com/NovruzIbrahimov/Furniture_ecommerce_project.git"
+          h3="Furniture"
+          p="Furniture Ecommerce"
         />
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          src={game}
+          link="https://github.com/NovruzIbrahimov/Shopify_project.git"
+          h3="Game Shopify"
+          p="Game Accessories"
         />
         <ProjectCard
-          src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Hipsster"
-          p="Glasses Shop"
-        />
-        <ProjectCard
-          src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="FitLift"
-          p="Fitness App"
+          src={fashion}
+          link="https://github.com/NovruzIbrahimov/Ecommerce-Project.git"
+          h3="Porto"
+          p="Fashion Shop"
         />
       </div>
     </section>
