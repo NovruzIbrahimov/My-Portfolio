6ec923247572aea9b5ccc87f6dda10d2d9d2cf88
NovruzIbrahimov <novruz.ibrahimov56@gmail.com>
2024-07-22 17:29:52 +0400

    some corrections were made and additions were made

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,12 +1,13 @@
-import styles from './SkillsStyles.module.css';
-import checkMarkIconDark from '../../assets/checkmark-dark.svg';
-import checkMarkIconLight from '../../assets/checkmark-light.svg';
-import SkillList from '../../common/SkillList';
-import { useTheme } from '../../common/ThemeContext';
+import styles from "./SkillsStyles.module.css";
+import checkMarkIconDark from "../../assets/checkmark-dark.svg";
+import checkMarkIconLight from "../../assets/checkmark-light.svg";
+import SkillList from "../../common/SkillList";
+import { useTheme } from "../../common/ThemeContext";
 
 function Skills() {
   const { theme } = useTheme();
-  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+  const checkMarkIcon =
+    theme === "light" ? checkMarkIconLight : checkMarkIconDark;
 
   return (
     <section id="skills" className={styles.container}>
@@ -16,13 +17,12 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="Bootstrap" />
-        <SkillList src={checkMarkIcon} skill="Ant Design" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
-        
+        <SkillList src={checkMarkIcon} skill="Git" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Responsive Web Design" />
+        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="Responsive Design" />
         <SkillList src={checkMarkIcon} skill="React Js" />
         <SkillList src={checkMarkIcon} skill="Next Js" />
       </div>
@@ -31,7 +31,7 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="React context" />
         <SkillList src={checkMarkIcon} skill="Redux Toolkit" />
         <SkillList src={checkMarkIcon} skill="RESTful API" />
-        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Ant Design" />
       </div>
     </section>
   );
